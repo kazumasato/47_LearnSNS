fdbfe534cfa26643c3805dda8c7e66f9f903a8d2
佐藤一馬 <>
2018-11-12 10:12:09 +0800

    削除機能実装

--- delete.php
+++ delete.php
@@ -1,4 +1,16 @@
 <?php
-
+require('dbconnect.php');
+//1.どの投稿を削除するか
+$feed_id=$_GET['feed_id'];
+//echo "<pre>";
+//var_dump($feed_id);
+//echo "</pre>";
+//die();
+//2.Delete処理
+$sql='DELETE FROM `feeds` WHERE `id`=?';
+$data=[$feed_id];
+$stmt=$dbh->prepare($sql);
+$stmt->execute($data);
+//3.timeline.phpに遷移
 header("Location: timeline.php");
 exit();
